5db5145ab11f2d372971b0101d77f5da7e18e48b
HimalayaITSolutions <64773151+HimalayaITSolutions@users.noreply.github.com>
2020-05-14 17:01:41 +0530

    Some Const correction to Lambda

--- chap6.cpp
+++ chap6.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 
-// Literals need nameapace as well
+// Literals need namespace as well
 using namespace std;
 
 // Templated Function.
@@ -35,7 +35,7 @@ int count2(Array &Seq, Val N)  // Get by reference
 }
 
 // Function objects
-// Implement Less than function in count for 
+// Implement "Less than function" in count for 
 // special types.
 // Pass it around as an object.
 template <typename T>
@@ -45,9 +45,13 @@ class Less_than
 private:
     const Value_type x;
 public:
-    Less_than(const Value_type& n) { x = n; }
+    Less_than(const Value_type n) 
+        :x{ n }                                     // Const can be added only in initializer list. cant do as in commented code
+    {
+        // x = n;
+    }
 
-    bool operator()(const Value_type& in){return in < x;}
+    bool operator()(const Value_type in){return in < x;}
 };
 
 // More predicates
@@ -56,19 +60,25 @@ class More_than
 {
     using Value_type = T;
 private:
-    const Value_type x;
+    Value_type x;
 public:
-    More_than(const Value_type& n) { x = n; }
+    More_than(Value_type n)
+        :x{ n }
+    {}
 
-    bool operator()(const Value_type& in) { return in > x; }
+    bool operator()(Value_type in) { return in > x; }
 };
 
+// This is still weird
 template <typename T>
 class Zeroes
 {
     using Value_type = T;
+    T a = 0;
 public:
-    bool operator()(const Value_type& in) { return in == 0; }
+    Zeroes(T a1) { a = a1; }
+    Zeroes() {}
+    bool operator()( Value_type in) { return (in == 0); }
 };
 
 
@@ -77,20 +87,27 @@ public:
 // We dont need to create many counts, just one count and multiple internal algo'es telling how to count.
 
 template <typename Seq, typename Predicate>
-int count3(Seq s, Predicate P)
+int count3(const Seq s, Predicate P)
 {
     int i = 0;
-    for (auto &a : s)
+    for (const auto &a : s)
     {
         if (P(a)) i++;
     }
+    return i;
 }
+
 
 void testTemplateFuncs()
 {
     std::vector<int> v1{ 5,6,7,8 };
     std::vector<double> v2{ 5.5,6.6,7.7,8.7 };
+    std::vector<int> v3{ 1, 0, 5, 7 };
+
+    // Specific instantiation
     int c1 = count<std::vector<int>, 6>(v1);
+
+
     // int c1 = count<6>(v1); // incorrect instantiation
     int c2 = count2(v1, 6); // Type deduced automatically
 
@@ -102,10 +119,23 @@ void testTemplateFuncs()
     // See how simple and intuitive code is now
     // Decouple data, algorithm and loop
 
+    // Try instantitate without Template argument
+    // cant as it needs some template if 
+    Zeroes Z(1);
+    //Zeroes<int> z1;
+
     // Called Policy objects as well
     int c3 = count3(v1, Less_than{ 6 });  // Should give same answer to c2.
-    int c3 = count3(v2, More_than{ 7.3}); // but we need a new count than count 2 for this function. Here count stays same.
+    int c4 = count3(v2, More_than{ 7.3}); // but we need a new func than count2 for " > case" function. Here, count stays same.
+    int cz = count3(v3, Z);
 
+    // The craziest, crappiest, lambda in c++
+    // Expands to function object above
+    // use only if simple
+    int x = 6;
+    int c5 = count3(v1, [&](int a) {return (a < x); });  // Capture locals by ref, capture X.
+    int c6 = count3(v2, [=](double a) {return (a < 7.5); }); // capture and reuse locals by value
+    int c7 = count3(v3, [](int a) {return (a == 0); }); // Corresponds to zeroes, no capture
 
 
 }
